a1c491db303d7732d540b4ae93f5d3c5cbd1893d
k <iliakripa@mail.ru>
2025-04-19 01:56:50 +0300

    add cors

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.bert import BertModel
 from src.config import settings
 from src.routers import routers_list
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/api"
+)
+
+origins = [
+    'http://194.87.248.63:3000',
+    'https://194.87.248.63:3000',
+    'http://localhost:3000',
+    'https://tunom.ru',
+    'http://tunom.ru'
+    'https://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.post('/question')
 def ask_question(question: str = Body(embed=True)):
